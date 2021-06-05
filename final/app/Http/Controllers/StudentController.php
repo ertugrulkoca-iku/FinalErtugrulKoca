642e5d86d018a105b581df909e45c3e20d7b29dc
Ertugrul Koca <1700004019@stu.iku.edu.tr>
2021-06-05 11:44:07 +0300

    yedinci commit

--- final/app/Http/Controllers/StudentController.php
+++ final/app/Http/Controllers/StudentController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    function ogrencikayitform()
+    {
+        return view('studentsave');
+    }
+
+    function ogrencikayit(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required',
+            'surname' => 'required',
+            'department' => 'required'
+        ]);
+
+        
+        $new = new Student();
+        $new->name = $request->name;
+        $new->surname = $request->surname;
+        $new->department = $request->department;
+        $save = $new->save();
+
+        if ($save) {
+            return back()->with('success', 'successfully');
+        } else {
+            return back()->with('fail', 'Something wrong');
+        }
+    }
+
 }
